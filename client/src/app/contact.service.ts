eaef4ac11004d7415cb4de8a140bd74deb73a73c
ayoub e <eayoub@gmail.com>
2018-05-25 15:17:19 +0100

    fix rgx error

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import {Http,Headers} from '@angular/http';
 import {Contact} from './contact';
-import 'rxjs/add/operator/map';
-import  'rxjs/observable/';
+import { map } from "rxjs/operators";
+
+
 
 @Injectable()
 export class ContactService {
 
-  constructor(private http:Http) { }
+    constructor(private http:Http) { }
 
-  getContacts(){
+    getContacts(){
+
     return this.http.get('http://localhost:3031/api/contact/')
-    .map( res => res.json());
-  }
+    .pipe(map( res => res.json()));
+    
+    }
+    
 }
